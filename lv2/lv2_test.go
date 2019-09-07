de699546b07671ee52446b58e32282a514bf6ec7
Michael Fisher <mfisher@kushview.net>
2019-09-07 08:53:49 -0400

    use lv2_test package in tests

--- lv2/lv2_test.go
+++ lv2/lv2_test.go
@@ -1,31 +1,33 @@
-package lv2
+package lv2_test
 
 import (
 	"testing"
+
+	"github.com/lvtk/go/lv2"
 )
 
 func TestFeatureNameEmpty(t *testing.T) {
-	feature := new(Feature)
+	feature := new(lv2.Feature)
 	if len(feature.URI) > 0 {
 		t.Errorf("URI not empty")
 	}
 }
 
 func TestFeatureDataNil(t *testing.T) {
-	feature := new(Feature)
+	feature := new(lv2.Feature)
 	if feature.Data != nil {
 		t.Errorf("Data not nil")
 	}
 }
 
 func TestFeatureSlice(t *testing.T) {
-	fs := NewFeatures()
+	fs := lv2.NewFeatures()
 	defer fs.Clear()
 	if fs.Size() != 0 {
 		t.Fatalf("size incorrect")
 	}
 
-	nf := new(Feature)
+	nf := new(lv2.Feature)
 	nf.URI = "http://hello.org"
 	nf.Data = nil
 	fs.Append(nf)
