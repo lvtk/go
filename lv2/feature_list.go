df7b7948da39d39ef40f2ca656f48fc5f78b6205
Michael Fisher <mfisher@kushview.net>
2019-09-13 09:29:03 -0400

    lv2: ability to reference a C-type feature array

--- lv2/feature_list.go
+++ lv2/feature_list.go
@@ -6,9 +6,11 @@ package lv2
 #include <stdint.h>
 
 // allocate new feature array with NULL-termination
-static LV2_Feature** alloc_features() {
-	LV2_Feature** fs = (LV2_Feature**) malloc (sizeof (LV2_Feature*));
-	fs[0] = NULL;
+static LV2_Feature** alloc_features(uint32_t size) {
+	if (size < 1)
+		size = 1;
+	LV2_Feature** fs = (LV2_Feature**) malloc (size * sizeof (LV2_Feature*));
+	fs[size - 1] = NULL;
 	return fs;
 }
 
@@ -23,6 +25,17 @@ static LV2_Feature** realloc_features(LV2_Feature** fs, uint32_t count) {
 // assign a feature to the array by index
 static void set_feature (LV2_Feature** fs, uint32_t index, LV2_Feature* f) {
 	fs[index] = f;
+}
+
+static LV2_Feature* get_feature (LV2_Feature** fs, uint32_t index) {
+	return fs[index];
+}
+
+static uint32_t feature_list_size (LV2_Feature** fs) {
+	uint32_t i = 0;
+	for (const LV2_Feature* f = fs[i]; f != NULL; ++i)
+		;
+	return i + 1;
 }
 
 */
@@ -44,9 +57,23 @@ type FeatureList struct {
 // NewFeatureList - create new feature list
 func NewFeatureList() *FeatureList {
 	f := new(FeatureList)
-	f.cfeats = C.alloc_features()
+	f.cfeats = C.alloc_features(1)
 	f.count = 1
 	return f
+}
+
+// NewFeatureListRef creates a new list from existing ref
+// @param ref MUST be a pointer to a null terminated LV2_Feature** array
+func NewFeatureListRef(ref unsafe.Pointer) *FeatureList {
+	fs := NewFeatureList()
+	cfs := (**C.LV2_Feature)(ref)
+	size := uint32(C.feature_list_size(cfs))
+	for i := uint32(0); i < size; i++ {
+		f := NewFeatureRef(unsafe.Pointer(C.get_feature(cfs, C.uint32_t(i))))
+		fs.Append(f)
+	}
+
+	return fs
 }
 
 // Free - This must be called when finished with the array or you will leak memory
@@ -81,7 +108,7 @@ func (f *FeatureList) Clear() {
 
 	f.Free()
 
-	f.cfeats = C.alloc_features()
+	f.cfeats = C.alloc_features(1)
 	f.count = 1
 	f.features = make([]*Feature, 0)
 }
